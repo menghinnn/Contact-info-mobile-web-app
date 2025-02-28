97174f82f4d8bee3b4cb6aad1dad4e8a2bd09ee0
menghinnn <107351518+menghinnn@users.noreply.github.com>
2025-02-28 12:19:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function retrieveContacts(){
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4){
             var contacts = JSON.parse(xhr.response).contacts;
-            displayContacts(contacts)
+            displayContacts(contacts);
 
             const localStorage= window.localStorage;
             if(localStorage){
